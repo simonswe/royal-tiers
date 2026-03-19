a68eaffba85e1260b224b3b27339f0e2012ddbd8
Simon Huang <simonhuang553@yahoo.ca>
2026-03-19 00:11:24 -0700

    fix prod

--- src/app/list/new/page.tsx
+++ src/app/list/new/page.tsx
@@ -2,6 +2,9 @@ import { redirect } from "next/navigation";
 import { prisma } from "@/lib/db";
 import { nanoid } from "nanoid";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export default async function NewTierListPage() {
   const shareSlug = nanoid(10);
   const tierList = await prisma.tierList.create({
